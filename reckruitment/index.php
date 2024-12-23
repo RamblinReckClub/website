8f2443603b72fe5d01ed5014b6487611f4034fd7
Gal Ovadia <gal.ovadia@gatech.edu>
2024-12-23 14:26:24 +0100

    reckruitment updates

--- reckruitment/index.php
+++ reckruitment/index.php
@@ -9,29 +9,28 @@
 <div class="container">
 <h2 class="mb-3">RECKruitment Process</h2>
     <img class="img-responsive mb-2" src="/img/why/RRCPolos21.png" width="100%" alt="Picture of the 2021 RRC Polos">
-    <p>Every spring semester, Ramblin’ Reck Club holds RECKruitment in order to take in a new class of probates (probationary members). These probates are selected from a pool of applicants who must first submit an application and attend a series of RECKruitment events.  Email <a href="mailto:membershipchair@reckclub.org">Eleanor Froula</a>, Ramblin' Reck Club Recruitment Chair, if you have questions or concerns. Find the breakdown of these events below:</p>
+    <p>Every spring semester, Ramblin’ Reck Club holds RECKruitment in order to take in a new class of probates (probationary members). These probates are selected from a pool of applicants who must first submit an application and attend a series of RECKruitment events.  Email <a href="mailto:membershipchair@reckclub.org">Simran Patel and Gal Ovadia</a>, Ramblin' Reck Club Recruitment Chairs, if you have questions or concerns. Find the breakdown of these events below:</p>
 
-    <h4 class="mb-3">Overview of the 2024 Application Process and Timeline</h4>
+    <h4 class="mb-3">Overview of the 2025 Application Process and Timeline</h4>
 
     <p class="mb-0">A full and complete application for membership includes:</p>
-<!--    <p class="mt-0 text-muted"><i>Note: submission deadline and all event dates TBD for 2020</i></p>-->
     <ul>
-        <li>A <a href="#written-application">written application</a> submitted by <strong>Wednesday, January 31st</strong></li>
+        <li>A <a href="#written-application">written application</a> submitted by <strong>Sunday, Februrary 2nd, 2025</strong></li>
         <li>Attendance at one of the three selected <a href="#gtky-events">“Get to Know You” (GTKY)</a> Events</li>
     </ul>
 
-    <p class="mb-0">Upon proper completion of the application, invitations will be sent out for the next recruitment events:</p>
+    <p class="mb-0">Upon proper completion of the application, invitations will be sent out for the next recruitment events (by invitation only):</p>
 
     <ul>
-        <li>Attendance at the <a href="#first-social">first social</a> on <strong>Monday, February 6th</strong></li>
-        <li><a href="#interviews">Interviews</a> (Individual and Group), occurring <strong> February 6th - February 8th</strong></li>
-        <li>Attendance at the <a href="#second-social">second social</a> on <strong>Saturday, February 10th</strong> (by invitation only)</li>
-        <li>Attendance at the all-club Spring Retreat (Date TBD if selected to be a 2024 probate)</li>
+        <li>Attendance at the <a href="#first-social">first social</a> on <strong>Monday, February 10th, 2025</strong></li>
+        <li><a href="#interviews">Interviews</a> (Individual and Group), occurring <strong> February 11th - February 13th, 2025</strong></li>
+        <li>Attendance at the <a href="#second-social">second social</a> on <strong>Saturday, February 15th</strong></li>
+        <li>Attendance at the all-club Spring Retreat (Date TBD if selected to be a 2025 probate)</li>
     </ul>
 
     <p>For details about each of the above requirements, check out the sections below or click on the requirement for more information!</p>
 
-    <p>Any absences or potential conflicts with the required events should be brought to the attention of <a href="mailto:membershipchair@reckclub.org">Eleanor Froula</a>, Ramblin’ Reck Club Recruitment Chair, no later than Wednesday, January 31st.</p>
+    <p>Any absences or potential conflicts with the required events should be brought to the attention of <a href="mailto:membershipchair@reckclub.org">Simran Patel and Gal Ovadia</a>, Ramblin’ Reck Club Recruitment Chairs, no later than February 2nd, 2025.</p>
 
 <h4 class="mb-2">Probate Requirements</h4>
 
@@ -40,7 +39,7 @@
 <ul>
     <li>Applicants may <strong>not</strong> graduate prior to December 2024.</li>
     <li>Applicants must be able to attend a weekly probate meeting on Monday evenings from 6-7 PM and then the full club meeting at 7 PM. Exceptions to this can be made, but very rarely.</li>
-    <li>Spring 2024 out-of-town co-ops and study abroad students are not eligible to apply this RECKruitment season. Fall 2024 out-of-town co-ops and study abroad students will be dealt with on a case by case basis. Email <a href="mailto:membershipchair@reckclub.org">Eleanor Froula</a>, Ramblin’ Reck Club Recruitment Chair, if you have questions or concerns.</li>
+    <li>Spring 2025 out-of-town co-ops and study abroad students are not eligible to apply this RECKruitment season. Fall 2025 out-of-town co-ops and study abroad students will be dealt with on a case by case basis. Email <a href="mailto:membershipchair@reckclub.org">Simran Patel and Gal Ovadia</a>, Ramblin’ Reck Club Recruitment Chairs, if you have questions or concerns.</li>
 </ul>
 
 <h4 class="mb-2">Tips for RECKruitment</h4>
@@ -57,64 +56,54 @@
 
 
 <!--<p>The 2024 Ramblin’ Reck Club application is now closed. Check back next December for 2025's application!</p>-->
-    <p>The 2024 Ramblin’ Reck Club application can be found <a href="https://docs.google.com/forms/d/e/1FAIpQLSccAlBLTfa8IXV50ks-Q0o3vvgpgwmtR6VDpzdzwVMnNeYAVg/closedform" target="blank">here</a>. A completed written application must be submitted no later than <strong>Wednesday, January 31st at 11:59pm</strong>.</p>
-
-    <!--<p>The 2024 Ramblin’ Reck Club application will open on January 1st, to be notified when applications open, sign up for this <a href="https://forms.gle/zrW66Pyrn8eGi7aV8">email list!</a> </p> -->
+    <p>The 2024 Ramblin’ Reck Club application can be found <a href="https://forms.gle/cdiEqBsiy84Dhbme6" target="blank">here</a>. A completed written application must be submitted no later than <strong>Sunday, February 2nd 2025 at 11:59pm</strong>.</p>
 
 
     <div class='row text-center'>
         <div class='col-sm col-xs-12 mb-5 mb-sm-0'>
-            <a class='btn btn-md btn-primary' href="https://docs.google.com/forms/d/e/1FAIpQLSccAlBLTfa8IXV50ks-Q0o3vvgpgwmtR6VDpzdzwVMnNeYAVg/closedform" target="blank"><strong>APPLY HERE!</strong></a>
+            <a class='btn btn-md btn-primary' href="https://forms.gle/cdiEqBsiy84Dhbme6" target="blank"><strong>APPLY HERE!</strong></a>
         </div>
     </div>
 
     <h4 class="mb-2" id="info-sessions">Information Sessions (Recommended)</h4>
 
-    <p>For anyone interested in learning more about Ramblin’ Reck Club, what we are involved in on campus, opportunities within the Club, and the recruitment process, we will be hosting information sessions before the application deadline closes. Pizza will be provided, so feel free to stop in and introduce yourself and get the low-down on all things RRC during the session. It is highly encouraged to attend one of these sessions to help you understand what this club is about, but if you cannot attend, this information will be provided after the meetings.</p>
+    <p>For anyone interested in learning more about Ramblin’ Reck Club, what we are involved in on campus, opportunities within the Club, and the recruitment process, we will be hosting information sessions before the application deadline closes. Feel free to stop in and introduce yourself and get the low-down on all things RRC during the session. It is highly encouraged to attend one of these sessions to help you understand what this club is about, but if you cannot attend, this information will be provided after the meetings.</p>
 
     <ul>
-        <li><strong>Wednesday, January 10th at 7:00pm-8:00pm</strong> in <a href="https://studentcenter.gatech.edu/exhibition-hall-map" target="blank">Buckhead meeting room</a> (Exhibition Hall)</li>
-        <li><strong>Thursday, January 18th at 6:00pm-7:00pm</strong> in <a href="https://studentcenter.gatech.edu/exhibition-hall-map" target="blank">Buckhead meeting room</a> (Exhibition Hall)</li>
-        <li><strong>Sunday, January 28th at 2:00pm-3:00pm</strong> on <a href="https://gatech.zoom.us/j/97512995783" target="blank">Zoom.</a> </li>
-        <!-- CHANGE ZOOM LINK -->
-        <!--    <li><strong>First Social: Monday, January 28th at 7:15pm (Mandatory)</strong></li>-->
-<!--  <li>Check back here in December 2024 for info session dates for the 2025 recruitment cycle!</li>-->
-</ul>
+        <li><strong>Tuesday, January 14th at 7:30pm-8:30pm</strong> in Atlantic Theater (Student Center)</li>
+        <li><strong>Wednesday, January 22nd at 5:00pm-6:00pm</strong> in <a href="https://studentcenter.gatech.edu/exhibition-hall-map" target="blank">Centennial meeting room</a> (Exhibition Hall)</li>
+        <li><strong>Thursday, January 30th at 6:00pm-7:00pm</strong> on Teams (link will be available here in days leading to the event).</li>
+    </ul>
 
 <h4 class="mb-2" id="gtky-events">RRC “Get To Know You” Events (Mandatory)</h4>
 
     <p>In order to provide applicants a casual setting to interact with Reck Clubbers and get a better feel for the kind of spirit Reck Club has for Georgia Tech, all applicants must attend one of the following Get To Know You events. Please note that applicants need only attend one of the following events in order to satisfy the recruitment requirements. Make sure to check in when you arrive, you do not need to stay for the entire event duration. </p>
 
     <ul>
-        <li><strong>Saturday, January 20th from 1:00pm-3:00pm</strong> outside the Reck Garage: Traditions Scavenger Hunt</li>
-        <li><strong>Tuesday, January 23rd from 7:00pm-9:00pm</strong> in the <a href="https://studentcenter.gatech.edu/student-center-maps" target="blank">Cypress Theater</a> (Student Center): Game Night</li>
-        <li><strong>Saturday, February 3rd from 5:30pm-8:00pm</strong> in the <a href="https://studentcenter.gatech.edu/student-center-maps" target="blank">Cypress Theater</a> (Student Center): Basketball Watch Party</li>
-        <!--    <li><strong>Interviews: Tuesday, January 29th through Thursday, January 31st from 5:30-9pm (Mandatory)</strong></li>-->
-        <!--    <li>Check back here in December 2019 for athletic social dates for the 2020 recruitment cycle!</li>-->
+    <li><strong>Saturday, January 25th</strong> Time and Location TBD.</li>
+    <li><strong>Tuesday, January 28th</strong> Time and Location TBD.</li>
+    <li><strong>Thursday, February 6th</strong> Time and Location TBD.</li>
     </ul>
 
 <h4 class="mb-2" id="first-social">First Social (Invite Only)</h4>
-    <p>Attending this social is your chance as an applicant to meet Reck Clubbers, get a feel for what Ramblin’ Reck Club is all about, ask any questions you may have about the Club or getting involved, and most importantly, sign up for an interview time. No need to stress about this event; be yourself and talk to anyone you do not already know! This event will be  on the evening of <strong>Monday, February 5th at The Alumni House.</strong></p>
-    <!-- <ul>
-        <li><strong>First Social: Monday, February 6th</strong></li>
-    </ul> -->
+    <p>Attending this social is your chance as an applicant to meet Reck Clubbers, get a feel for what Ramblin’ Reck Club is all about, ask any questions you may have about the Club or getting involved, and most importantly, sign up for an interview time. No need to stress about this event; be yourself and talk to anyone you do not already know! This event will be  on the evening of <strong>Monday, February 10th.</strong> Location informatio will be sent to selected applicants.</p>
     <p>There are two time slots for the firt social: <strong>7:00PM - 8:15PM</strong> and <strong>8:45PM - 10:00PM </strong>. Once you submit the written application, you will receive an email instructing you to fill out your availability.  An invitation to one of the two time slots based on reported availability will be sent out after the application period has ended!</p>
 
 <h4 class="mb-2" id="interviews">Interviews (Invite Only)</h4>
 
-    <p>The interview portion of RECKruitment is designed to allow for Reck Clubbers to get to know applicants on a more personal basis. We just want to give each applicant the time to express their ideas, commitment, and personality. The interview portion consists of an individual interview and a group interview. Interviewees will sign up for an interview time slot during check-in at the first social. All interview time slots will be between 6:00pm and 10:00pm on February 6th, 7th, and 8th. Interviews will be conducted in the Center for Student Engagement. </p>
-
-     <!-- <ul>
-        <li><strong>Tuesday, February 7th from 6:00pm-10:00pm</strong></li>
-        <li><strong>Wednesday, February 8th from 5:00pm-10:00pm</strong></li>
-        <li><strong>Thursday, February 9th from 8:00pm-11:00pm</strong></li>
-    </ul> -->
+    <p>The interview portion of RECKruitment is designed to allow for Reck Clubbers to get to know applicants on a more personal basis. We want to give each applicant the time to express their ideas, commitment, and personality. The interview portion consists of an individual interview and a group interview. Interviewees will sign up for an interview time slot during check-in at the first social.</p>
+     <ul>
+        <li><strong>Tuesday, February 11th from 7:30pm-11:00pm</strong></li>
+        <li><strong>Wednesday, February 12th from 7:30pm-11:00pm</strong></li>
+        <li><strong>Thursday, February 13th from 7:30pm-11:00pm</strong></li>
+    </ul>
+    <p>Applicants selected for interviews will be notified with further details.</p>
 
 <h4 class="mb-2" id="second-social">Second Social (Invite Only)</h4>
-    <p>After all interviews have been conducted, applicants will be notified whether they will advance to the second round of RECKruitment. During this second round, applicants will attend a mandatory second social held from <strong>1:00PM - 3:30PM on Saturday, February 10th</strong>. Further details for this event will be sent out to those invited applicants only.</p>
+    <p>After all interviews have been conducted, applicants will be notified whether they will advance to the next round of RECKruitment. During this second round, applicants will attend a mandatory second social held from <strong>12:00PM - 3:00PM on Saturday, February 15th</strong>. Further details for this event will be sent out to those invited applicants only.</p>
 
 <hr class="mb-3"> <!--    adds dividing line-->
-    <p>Any time conflicts with these events should be brought to the attention of <a href="mailto:membershipchair@reckclub.org">Eleanor Froula</a>, Ramblin’ Reck Club Recruitment Chair, no later than <strong>Wednesday, January 31st</strong>.</p>
+    <p>Any time conflicts with these events should be brought to the attention of <a href="mailto:membershipchair@reckclub.org">Simran Patel and Gal Ovadia</a>, Ramblin’ Reck Club Recruitment Chairs, no later than <strong>February 2nd</strong>.</p>
 
 
     <p>We wish you the best of luck during this recruitment process, and we look forward to meeting you soon. Thank you for considering joining the Ramblin’ Reck Club!</p>
