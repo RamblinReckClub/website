1e068b7a2d87a11084ae2dd4ca9bef8c71b37377
Steven Baker <42903115+sbdevelops@users.noreply.github.com>
2026-02-01 23:02:52 -0500

    Make password reset default for new users

--- createMember.php
+++ createMember.php
@@ -18,8 +18,9 @@
 	
 	if($var == false) {
         $newPasswordHash = password_hash($_POST['password'], PASSWORD_BCRYPT);
-		$query = $db->prepare("INSERT INTO Member (username, password, firstName, lastName, status) VALUES (:username, :password, :firstName, :lastName, :status)");
-		$query->execute(array('username'=>$_POST['username'], 'password'=>$newPasswordHash, 'firstName'=>$_POST['firstName'], 'lastName'=>$_POST['lastName'], 'status'=>$_POST['status']));
+        $oneYearAgo = date('Y-m-d H:i:s', strtotime('-1 year'));
+		$query = $db->prepare("INSERT INTO Member (username, password, passwordType, firstName, lastName, status, passwordExpiration) VALUES (:username, :password, 'bcrypt', :firstName, :lastName, :status, :passwordExpiration)");
+		$query->execute(array('username'=>$_POST['username'], 'password'=>$newPasswordHash, 'firstName'=>$_POST['firstName'], 'lastName'=>$_POST['lastName'], 'status'=>$_POST['status'], 'passwordExpiration'=>$oneYearAgo));
 	
 		echo "<h3>Member Created</h3>";
 		echo "<meta http-equiv=\"refresh\" content=\"2; url=manageMembers.php\">";
